297c8f0b9f5d1cc975fc5bca755c3d41164242d2
Matthew Olivarez <29942482+MatthewOlivarez@users.noreply.github.com>
2022-04-25 23:38:42 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -2,18 +2,22 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const axios = require('axios')
-const port = process.env.PORT || 3000 // saving port to be used in server into variable
+const port = process.env.PORT || 80 // saving port to be used in server into variable
+require('dotenv').config()
+//console.log(process.env)
+
+// retrieving hidden api keys
+const GEO_KEY = process.env.GEOAPIFY_KEY 
+const NEWS_KEY = process.env.NEWS_KEY
+const RAPID_KEY = process.env.RAPID_KEY
 
 
 app.set('view engine', 'ejs') // sets engine for ejs formatting 
 
 // tells express to search for static files in views directory, static files are html files
-//app.use(express.static(path.join(__dirname + '/views'))) 
-app.set('views', [
-    path.join(__dirname, '/views'),
-    path.join(__dirname, '/views/news'),
-    path.join(__dirname, '/views/game')
-])
+app.use(express.static(path.join(__dirname + '/views'))) 
+//app.set('views', path.join(__dirname, '/views'))
+
 app.use(express.urlencoded({ extended: true })) // parses requests in JSON format
 app.use(express.json()) // similar to above, ensures we use JSON format for data from api's
 
@@ -23,14 +27,17 @@ app.use(express.json()) // similar to above, ensures we use JSON format for data
 app.use(express.static(path.join(__dirname + '/public')))
 
 // both of below statements are meant to incorporate three.js library for 3-d functionality
-app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build')));
-app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')));
+app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build')))
+app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')))
 
 
 let newsStories = [] // this will be array of objects to store news stories
 let newsCollected = true 
+let countryFacts = []
 let countryCode // will store country code returned from geoapify api
 let countryName // will store country name returned from geoapify api
+let cultureData = []
+let healthData = []
 
 // "get" route that targets our home/landing page, no matter the result fo the if statement, 
 // we render the index file in views directory
@@ -50,10 +57,17 @@ app.get('/', (req, res) => {
     res.render('index') // landing page
 })
 
+// get route, retrieves game-home.ejs
 app.get('/game', (req, res) => {
-    res.render('game-home') // news globe page aka informational page
+    res.render('game-home')
 })
 
+// get route, retrieves game-globe.ejs
+app.get('/game-globe', (req, res) => {
+    res.render('game-globe')
+})
+
+// get route, retrieves news-globe.ejs
 app.get('/news', (req, res) => {
     res.render('news-globe') // news globe page aka informational page
 })
@@ -67,19 +81,27 @@ app.get('/news', (req, res) => {
 app.post('/search', async (req, res) => { // post request to route search
     const { latitude, longitude } = req.body // destructuring or unpacking object
     //console.log(latitude + " " + longitude)
-    // console.log(req.body)
-    await axios.get(`https://api.geoapify.com/v1/geocode/reverse?lat=${latitude}&lon=${longitude}&apiKey=6650f1fc646f45b9ac9f90d91eec2078`) 
+    console.log("Request data taken from form...\n")
+    console.log(req.body + "\n\n")
+    // axios get request to geoapify api to reverse geocode the latitude and longitude pulled from the requested form
+    await axios.get(`https://api.geoapify.com/v1/geocode/reverse?lat=${latitude}&lon=${longitude}&apiKey=${GEO_KEY}`) 
         .then( (response) => {
             //console.log(response.data.features[0].properties.country_code)
-            countryCode = response.data.features[0].properties.country_code;
-            countryName = response.data.features[0].properties.country;
+            countryCode = response.data.features[0].properties.country_code; // storing country code from api
+            countryName = response.data.features[0].properties.country; // storing country name from api
+            console.log("Geoapify API data response...\n")
+            console.log(response.data.features[0].properties + "\n\n")
         })
         .catch( (error) => {
             // handle error
             console.log(error);
         })
-    await axios.get(`https://newsapi.org/v2/top-headlines?country=${countryCode}&apiKey=fb4e52ef50da4fe1ba96b5b92562f13b`)
+    // axios get request to news api using country code and country name that were just retrieved
+    await axios.get(`https://newsapi.org/v2/top-headlines?country=${countryCode}&apiKey=${NEWS_KEY}`)
         .then( (response) => {
+            console.log("News API response below...\n")
+            console.log(response.data.articles + "\n\n")
+            // storing top 9 news stories
             newsStories = []
             for (let i = 0; i < 10; i++)
             {
@@ -92,6 +114,72 @@ app.post('/search', async (req, res) => { // post request to route search
             // handle error
             console.log(error);
         })
+    // axios get request to rest countries api using country code variable
+    await axios.get(`https://restcountries.com/v3.1/alpha/${countryCode}`)
+        .then((response) => {
+            console.log("Country facts API response below...\n")
+            console.log(response.data + "\n\n")
+            countryFacts = []
+            //console.log(response.data.articles[i])
+            countryFacts.push(response.data) // storing entirety of response to use in results page
+        })
+        .catch((error) => {
+            // handle error
+            console.log(error);
+        })
+    var options = {
+        method: 'GET',
+        url: 'https://spotify23.p.rapidapi.com/charts/',
+        params: { type: 'regional', country: countryCode, recurrence: 'daily', date: 'latest' },
+        headers: {
+            'X-RapidAPI-Host': 'spotify23.p.rapidapi.com',
+            'X-RapidAPI-Key': `${RAPID_KEY}`
+        }
+    }
+    // axios get request to spotify api 
+    await axios.request(options)
+        .then(function (response) {
+            console.log("Culture API data below...\n")
+            console.log(response.data + "\n\n")
+            cultureData = [];
+            for (let i = 0; i < 6 ; i++) {
+                cultureData.push(response.data.content[i]) // storing top 5 results in array
+            }
+        })
+        .catch(function (error) {
+            console.error(error);
+        });
+    var options = {
+        method: 'GET',
+        url: 'https://countrystat.p.rapidapi.com/coronavirus/who_latest_stat_by_country.php',
+        params: { country: countryName },
+        headers: {
+            'X-RapidAPI-Host': 'countrystat.p.rapidapi.com',
+            'X-RapidAPI-Key': `${RAPID_KEY}`
+        }
+    };
+    // axios get request to country stat api
+    await axios.request(options)
+        .then(function (response) {
+            console.log("Health API data below...\n")
+            console.log(response.data + "\n\n")
+            healthData = [];
+            //  for (let i = 0; i < 6; i++) {
+            // pushing the following data into the array
+            healthData.push(response.data.recordDate)
+            healthData.push(response.data.population)
+            healthData.push(response.data.lifeExpectancy)
+            healthData.push(response.data.totalDeaths)
+            healthData.push(response.data.newDeaths)
+            healthData.push(response.data.totalCases)
+            healthData.push(response.data.newCases)
+                // const myJSON = JSON.stringify(obj);
+                // localStorage.setItem("testJSON", myJSON);
+            //   }
+        })
+        .catch(function (error) {
+            console.error(error);
+        });
     //res.send('worked')
     res.redirect('/results') // redirects to results route, which will display results to user
 })
@@ -100,7 +188,7 @@ app.post('/search', async (req, res) => { // post request to route search
 // "get" route that targets our results page, and will render results file from views directory
 app.get('/results', (req, res) => {
     //res.render('results')
-    res.render('results', { newsStories, countryName }) // renders results, { newsStories, countryName } this will pass the newsStories array and country name variable into the file using the ejs package, which is for templating and sending data into files
+    res.render('results', { newsStories, countryName, countryFacts, cultureData, healthData }) // renders results, { newsStories, countryName } this will pass the newsStories array and country name variable into the file using the ejs package, which is for templating and sending data into files
 })
  
 
